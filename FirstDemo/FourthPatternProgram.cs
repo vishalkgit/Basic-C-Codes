0fc58beacc789f1e0a6e843b3a47f3a25f4c4a4f
vishalkgit <kalevishal1012@gmail.com>
2024-10-17 16:25:13 +0530

    new

--- FirstDemo/FourthPatternProgram.cs
+++ FirstDemo/FourthPatternProgram.cs
@@ -9,22 +9,22 @@ namespace FirstDemo
 {
     internal class FourthPatternProgram
     {
-        //static void Main(string[] args)
-        //{
-        //    int rows = 7;
-        //    for(int i=1;i<=rows;i++)
-        //    {
-        //        for(int j=1;j<=rows-i;j++)
-        //        {
-        //            Console.Write(" ");
-        //        }
-        //        for(int j=1;j<=i;j++)
-        //        {
-        //            Console.Write(j);
-        //        }
-        //        Console.WriteLine();
-        //    }
-            
+        static void Main(string[] args)
+        {
+            int rows = 7;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = i; j < rows; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(k+" ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
-
+}
--- FirstDemo/SixthPattern.cs
+++ FirstDemo/SixthPattern.cs
@@ -9,26 +9,26 @@ namespace FirstDemo
 {
     internal class SixthPattern
     {
-        static void Main(string[] args)
-        {
-            int rows = 5;
-            for(int i=1;i<=rows;i++)
-                {
-                for(int j=1;j<=i;j++)
-                { 
-                    Console.Write("*");
-                }
-                 Console.WriteLine();
-            }
-                    for(int i=rows;i>=1;i--)
-                    {
-                        for(int j=1;j<=i;j++)
-                        {
-                            Console.Write("*");
-                        }
-                        Console.WriteLine();
+        //static void Main(string[] args)
+        //{
+        //    int rows = 5;
+        //    for(int i=1;i<=rows;i++)
+        //        {
+        //        for(int j=1;j<=i;j++)
+        //        { 
+        //            Console.Write("*");
+        //        }
+        //         Console.WriteLine();
+        //    }
+        //            for(int i=rows;i>=1;i--)
+        //            {
+        //                for(int j=1;j<=i;j++)
+        //                {
+        //                    Console.Write("*");
+        //                }
+        //                Console.WriteLine();
 
-                    }
-        }
+        //            }
+        //}
     }
 }
