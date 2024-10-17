565c9f095f38887d4217692b3bb7e10fcd273d75
vishalkgit <kalevishal1012@gmail.com>
2024-10-17 13:04:22 +0530

    added PrintOutput

--- FirstDemo/FibonacciSeries.cs
+++ FirstDemo/FibonacciSeries.cs
@@ -8,29 +8,29 @@ namespace FirstDemo
 {
     internal class FibonacciSeries
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please Enter a Number of terms");
-            int terms=Convert.ToInt32(Console.ReadLine());
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Please Enter a Number of terms");
+        //    int terms=Convert.ToInt32(Console.ReadLine());
 
-            if(terms<1)
-            {
-                Console.WriteLine("Please enter terms above 1");
-            }
+        //    if(terms<1)
+        //    {
+        //        Console.WriteLine("Please enter terms above 1");
+        //    }
 
 
-            int a = 0,b = 1;
+        //    int a = 0,b = 1;
 
-            for(int i=0;i< terms;i++)
-            {
-                Console.WriteLine(a);
-                int next = a + b;
-                a = b;
-                b=next;
+        //    for(int i=0;i< terms;i++)
+        //    {
+        //        Console.WriteLine(a);
+        //        int next = a + b;
+        //        a = b;
+        //        b=next;
 
-            }
+        //    }
 
             
-        }
+        //}
     }
 }
